e78bb6eb50af7c848e357840557c8401efdffa71
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2025-11-05 21:27:35 +0500

    fix

--- src/components/FooterBanner.tsx
+++ src/components/FooterBanner.tsx
@@ -4,7 +4,7 @@ const FooterBanner = () => {
   return (
     <footer className="flex flex-col sm:flex-row-reverse items-center justify-center sm:justify-around gap-10 sm:py-8 bg-cyan text-white overflow-hidden">
       {/* <footer className="flex flex-col-reverse items-center justify-center py-8 bg-cyan text-white overflow-hidden"> */}
-      <div className="object-contain w-full sm:w-96 h-full sm:h-80">
+      <div className="object-contain w-full sm:w-96 h-full sm:h-56">
         <img
           src="	https://www.lurity.com/images/cta_image_mobile.png"
           alt="Background"
@@ -13,8 +13,8 @@ const FooterBanner = () => {
       </div>
 
       <div className="px-2 ">
-        <div className="flex flex-col items-center lg:items-start space-y-6 mb-20 sm:mb-0">
-          <h2 className="text-5xl md:text-5xl lg:text-6xl text-center font-black text-black">
+        <div className="flex flex-col items-center lg:items-start space-y-10 mb-20 sm:mb-0">
+          <h2 className="text-5xl md:text-5xl lg:text-6xl text-center font-extrabold text-black">
             Try it effectively
           </h2>
           <Button className="bg-[#ffd503] w-fit text-black hover:bg-[#ffd503]/90 font-bold px-6 py-8 text-lg rounded-[10px] shadow-2xl">
